a988d59e6c8ceb7b6e9cd8c399ea71a2004d1b65
shjshshsjsu <122176104+shjshshsjsu@users.noreply.github.com>
2023-01-29 16:00:46 +0300

    Update Center Lib Example.lua

--- Center Lib Example.lua
+++ Center Lib Example.lua
@@ -2,46 +2,19 @@ local SimpleShindoUi = loadstring(game:HttpGet("https://raw.githubusercontent.co
 
 local Main = SimpleShindoUi:new()
 
-local Tab = Main:Tap('Centerhub')
+local Tab = Main:Tap('Wreston Hub')
 
 local page = Tab:page()
 
-local Labelz = page:Label('Label!')
+local Labelz = page:Label('Fly Gui!')
 
-page:Toggle('Toggle!',false,function(t)
 
-    print(t)
-
-end)
-
-page:Button('Button!',function()
+page:Button('Fly!',function()
 
     print('t')
 
 end)
 
-page:Slider('Slider!',1,100,20,function(t)
 
-    print(t)
 
-end)
 
-local drope = page:drop('Dropdown!',"Nothings",false,{'DinoHub','CenterHub'},function(t)
-
-    print(t) -- ห้ามนำไปแจกต่อนะจ๊ะ XD
-
-end)
-
-local page2 = Tab:page()
-
-page2:Button('Clear Dropdown!',function()
-
-    drope:clear()
-
-end)
-
-page2:Button('Refresh Dropdown!',function()
-
-    drope:add({"Simple"}) -- มันทำไงวะ -- ห้ามนำไปแจกต่อนะจ๊ะ XD
-
-end)
