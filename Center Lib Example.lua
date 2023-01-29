32ad00608ced82ffa0862b54ab28a4ef0c165fcb
shjshshsjsu <122176104+shjshshsjsu@users.noreply.github.com>
2023-01-29 19:20:37 +0300

    Update Center Lib Example.lua

--- Center Lib Example.lua
+++ Center Lib Example.lua
@@ -6,7 +6,7 @@ local Tab = Main:Tap('Wreston Hub')
 
  page = Tab:page()
 
-local Labelz = page:Label('Fly Gui!
+local Labelz = page:Label('Fly Gui!) 
 
 Gui Owner Youtube Account: me ozoneYT')
 
@@ -15,5 +15,43 @@ page:Button('Fly!',function()
 
     loadstring("\108\111\97\100\115\116\114\105\110\103\40\103\97\109\101\58\72\116\116\112\71\101\116\40\40\39\104\116\116\112\115\58\47\47\103\105\115\116\46\103\105\116\104\117\98\117\115\101\114\99\111\110\116\101\110\116\46\99\111\109\47\109\101\111\122\111\110\101\89\84\47\98\102\48\51\55\100\102\102\57\102\48\97\55\48\48\49\55\51\48\52\100\100\100\54\55\102\100\99\100\51\55\48\47\114\97\119\47\101\49\52\101\55\52\102\52\50\53\98\48\54\48\100\102\53\50\51\51\52\51\99\102\51\48\98\55\56\55\48\55\52\101\98\51\99\53\100\50\47\97\114\99\101\117\115\37\50\53\50\48\120\37\50\53\50\48\102\108\121\37\50\53\50\48\50\37\50\53\50\48\111\98\102\108\117\99\97\116\111\114\39\41\44\116\114\117\101\41\41\40\41\10\10")()
 
+end) 
+
+
+ page2 = Tab:page()
+
+local Labelz = page2:Label('No Kick!) 
+
+')
+
+page2:Button('Active No Kick!',function()
+local players = game:GetService("Players")
+
+local lp = players.LocalPlayer
+
+local gmt = getrawmetatable(game)
+
+local oldNamecall = gmt.__namecall
+
+setreadonly(gmt, false)
+
+gmt.__namecall = newcclosure(function(Self, ...)
+
+    local args = {...}
+
+    local namecallMethod = getnamecallmethod()
+
+    
+
+    if not checkcaller() and namecallMethod == "kick" or namecallMethod == "Kick" then
+
+        return nil
+
+    end
+
+    return oldNamecall(Self, ...)
+
 end)
+
+end) 
 
