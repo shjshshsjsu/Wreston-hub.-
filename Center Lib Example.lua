6bfe8c820e16c5c4e107f828741ef98722108969
shjshshsjsu <122176104+shjshshsjsu@users.noreply.github.com>
2023-01-30 19:16:57 +0300

    Update Center Lib Example.lua

--- Center Lib Example.lua
+++ Center Lib Example.lua
@@ -1,21 +1,79 @@
-local SimpleShindoUi = loadstring(game:HttpGet("https://raw.githubusercontent.com/naypramx/Ui__Project/Script/SimpleShindoUi"))()
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shjshshsjsu/Roblox-UI-Libs/main/Orion%20Lib/ejsjs')))()
 
-local Main = SimpleShindoUi:new()
+OrionLib:MakeNotification({
 
-local Tab = Main:Tap('Wreston Hub')
+	Name = "Wreston Hub",
 
- page = Tab:page()
+	Content = "Welcome To Wreston Hub! Have Fun! ",
 
-local Labelz = page:Label('Fly Gui!) 
+	Image = "rbxassetid://4483345998",
 
-Gui Owner Youtube Account: me ozoneYT')
+	Time = 5
 
+})
 
-page:Button('Fly!',function()
+local Window = OrionLib:MakeWindow({Name = "Wreston Hub", HidePremium = false, SaveConfig = true, ConfigFolder = "KryptedSaves"})
 
-    loadstring("\108\111\97\100\115\116\114\105\110\103\40\103\97\109\101\58\72\116\116\112\71\101\116\40\40\39\104\116\116\112\115\58\47\47\103\105\115\116\46\103\105\116\104\117\98\117\115\101\114\99\111\110\116\101\110\116\46\99\111\109\47\109\101\111\122\111\110\101\89\84\47\98\102\48\51\55\100\102\102\57\102\48\97\55\48\48\49\55\51\48\52\100\100\100\54\55\102\100\99\100\51\55\48\47\114\97\119\47\101\49\52\101\55\52\102\52\50\53\98\48\54\48\100\102\53\50\51\51\52\51\99\102\51\48\98\55\56\55\48\55\52\101\98\51\99\53\100\50\47\97\114\99\101\117\115\37\50\53\50\48\120\37\50\53\50\48\102\108\121\37\50\53\50\48\50\37\50\53\50\48\111\98\102\108\117\99\97\116\111\114\39\41\44\116\114\117\101\41\41\40\41\10\10")()
+--Player Tab--
 
-end) 
+local PlayerTab = Window:MakeTab({
 
+	Name = "Player",
 
- 
+	Icon = "rbxassetid://4483345998",
+
+	PremiumOnly = false
+
+})
+
+local PlayerSection = PlayerTab:AddSection({
+
+	Name = "Player"
+
+})
+
+PlayerSection:TextLabel({
+
+	Name = "Coming Soon",	
+
+        
+
+  	end    
+
+})
+
+--Player Tab End--
+
+--Settings Tab--
+
+local SettingsTab = Window:MakeTab({
+
+	Name = "Settings",
+
+	Icon = "rbxassetid://4483345998",
+
+	PremiumOnly = false
+
+})
+
+local SettingsSection = SettingsTab:AddSection({
+
+	Name = "Settings"
+
+})
+
+SettingsSection:AddButton({
+
+	Name = "Gui Close",
+
+	Callback = function()
+
+        OrionLib:Destroy()
+
+  	end    
+
+})
+
+--Settings End--
+
+OrionLib:Init() --UI Lib 
