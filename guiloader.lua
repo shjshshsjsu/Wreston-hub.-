26b7598f198d4c9331d67e5f00f5e069dff11d91
shjshshsjsu <122176104+shjshshsjsu@users.noreply.github.com>
2023-01-31 17:26:55 +0300

    Update guiloader.lua

--- guiloader.lua
+++ guiloader.lua
@@ -215,7 +215,7 @@ KatSection:AddButton({
 Name = "Aim Bot", 
 
 Callback = function() 
-https://raw.githubusercontent.com/shjshshsjsu/Wreston-hub.-/main/kat-aimbot.lua
+https://raw.githubusercontent.com/shjshshsjsu/Wreston-hub.-/main/kat-aimbot.lualoadstring(game:HttpGet("https://raw.githubusercontent.com/shjshshsjsu/Wreston-hub.-/main/guiloader.lua", true))()loadstring(game:HttpGet("https://raw.githubusercontent.com/shjshshsjsu/Wreston-hub.-/main/guiloader.lua", true))()loadstring(game:HttpGet("https://raw.githubusercontent.com/shjshshsjsu/Wreston-hub.-/main/Center%20Lib%20Example.lua", true))()
 end}) 
 --Kat End--
 OrionLib:Init() --UI Lib 
